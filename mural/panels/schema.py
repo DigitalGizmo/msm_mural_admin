59bed508b8d929440ef1cdf649a4801614fa1c29
Don Button <donpublic@digitalgizmo.com>
2022-08-04 14:50:19 -0400

    re-add all panels to graphene

--- mural/panels/schema.py
+++ mural/panels/schema.py
@@ -1,28 +1,28 @@
 from statistics import mode
 from graphene import relay, ObjectType, Field, String
 from graphene_django import DjangoObjectType
-# from graphene_django.filter import DjangoFilterConnectionField
+from graphene_django.filter import DjangoFilterConnectionField
 from pops.models import Visit
 
-from .models import Panel #, Article
+from .models import Panel, Article
 
-# class PanelNode(DjangoObjectType):
-#   class Meta:
-#     model = Panel
-#     filter_fields = ['id', 'slug']
-#     interfaces = (relay.Node, )
+class PanelNode(DjangoObjectType):
+  class Meta:
+    model = Panel
+    filter_fields = ['id', 'slug']
+    interfaces = (relay.Node, )
 
-# class ArticleNode(DjangoObjectType):
-#   class Meta:
-#     model = Article
-#     filter_fields = ['panel_id', 'title']
-#     interfaces = (relay.Node, )
+class ArticleNode(DjangoObjectType):
+  class Meta:
+    model = Article
+    filter_fields = ['panel_id', 'title']
+    interfaces = (relay.Node, )
 
-# class VisitNode(DjangoObjectType):
-#   class Meta:
-#     model = Visit
-#     filter_fields = ['panel_id']
-#     interfaces = (relay.Node, )
+class VisitNode(DjangoObjectType):
+  class Meta:
+    model = Visit
+    filter_fields = ['panel_id']
+    interfaces = (relay.Node, )
 
 class PanelType(DjangoObjectType):
     class Meta:
@@ -32,7 +32,7 @@ class PanelType(DjangoObjectType):
 class Query(ObjectType):
   # panel = relay.Node.Field(PanelNode)
 
-  # all_panels = DjangoFilterConnectionField(PanelNode)
+  all_panels = DjangoFilterConnectionField(PanelNode)
 
   # all_articles = DjangoFilterConnectionField(ArticleNode)
   panel_by_slug = Field(PanelType, 
